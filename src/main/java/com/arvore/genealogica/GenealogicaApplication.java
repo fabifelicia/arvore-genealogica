e16e9e49f181f5e70be39bf7cca82dd025c055a0
Fabiana Barreto <39680930+fabifelicia@users.noreply.github.com>
2025-07-04 10:27:53 -0300

    Add files
    Add files to the project

---
+++ src/main/java/com/arvore/genealogica/GenealogicaApplication.java
@@ -0,0 +1,13 @@
+package com.arvore.genealogica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenealogicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenealogicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arvore/genealogica/GenealogicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arvore.genealogica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenealogicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
